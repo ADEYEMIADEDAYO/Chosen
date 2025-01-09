8627e9920d1cad8a491aabb7327229ffac5245ee
adeyemiadedayo@ <adeyemiadedayo@>
2025-01-09 14:19:15 +0100

    first commit

---
+++ namer.js
@@ -0,0 +1,22 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+    id: 2,
+    text: 'Meeting with boss',
+    isCompleted: true
+},
+{
+    id: 3,
+    text: 'Dentist app',
+    isCompleted: false
+}
+];
+
+for(let i = 0; i < todos.length; i++) {
+console.log(todos[i].text);
+}
+    
